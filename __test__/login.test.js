0e0a2f4d781c0c9e2ab683085c4629f911825b05
eduardo azevedo <du-eduardo10@hotmail.com>
2021-12-09 19:58:58 -0300

    disgraça rapaiz

--- __test__/login.test.js
+++ __test__/login.test.js
@@ -1,13 +1,21 @@
 /* global __DEV__ */
 import React from 'react'
 import { render } from '@testing-library/react-native'
-//import Relatorio from '../screens/Relatorios/Relatorios'
-const {Relatorio} = require('../screens/Relatorios/Relatorios')
+import Relatorio from '../screens/Relatorios/Relatorios'
+// const {Relatorio} = require('../screens/Relatorios/Relatorios')
+
+jest.mock('../screens/Relatorios/Relatorios', () => '../screens/Relatorios/Relatorios');
+
+
 describe('Login', () => {
     test('should render', () => {
         const navigation = { navigate: () => { } }
-        const { toJSON } = render(<Relatorio navigation={navigation} />)
+        const { toJSON } = render(<Relatorio />)
 
         expect(toJSON()).toMatchSnapshot()
     })
 })
+
+// it('works', () => {
+//     expect(1).toBe(1);
+//   });
---
+++ screens/Login/testando.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { View, Text } from 'react-native-web'
+
+
+export default function Testando() {
+    return (
+        <View>
+            <View >
+                <Text>
+                    Problemas para se conectar?
+                </Text>
+            </View>
+            <View >
+                <Text >
+                    Contate a casa do Oleo
+                </Text>
+            </View>
+        </View>
+    )
+}
