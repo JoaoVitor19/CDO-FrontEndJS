0ebfac30b24abc75d18e33068c1c0d4212f01eaa
eduardo azevedo <du-eduardo10@hotmail.com>
2021-12-09 17:21:40 -0300

    teste

--- __test__/login.test.js
+++ __test__/login.test.js
@@ -1,11 +1,12 @@
+/* global __DEV__ */
 import React from 'react'
 import { render } from '@testing-library/react-native'
-import Login from '../screens/Login/Login'
-
+//import Relatorio from '../screens/Relatorios/Relatorios'
+const {Relatorio} = require('../screens/Relatorios/Relatorios')
 describe('Login', () => {
     test('should render', () => {
         const navigation = { navigate: () => { } }
-        const { toJSON } = render(<Login navigation={navigation} />)
+        const { toJSON } = render(<Relatorio navigation={navigation} />)
 
         expect(toJSON()).toMatchSnapshot()
     })
