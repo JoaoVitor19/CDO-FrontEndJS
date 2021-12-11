cabc1adc8206ebc74bc9c759d6e7527b28bde392
gustavo <gustavoantonio.pp@outlook.com>
2021-12-11 01:41:53 -0300

    fixed stylization

--- screens/Veiculos/Veiculos.jsx
+++ screens/Veiculos/Veiculos.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Provider } from 'react-native-paper';
 import { StyleSheet, Text, View, SafeAreaView, ScrollView, TextInput } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
-import ModalVeiculos from '../../components/ModelVeiculos';
+import ModalVeiculos from '../../components/ModalVeiculos';
 import NewVeiculo from '../../components/NewVeiculo';
 import { Portal, Modal, Button, IconButton } from 'react-native-paper';
 
